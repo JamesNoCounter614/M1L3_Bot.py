8fca84d6b9e6f85cc1dbd70400c961522f7fc535
JamesNoCounter614 <Alexanderjames.susilo@gmail.com>
2024-12-30 17:16:43 +0700

    Update and rename README.md to READ.ME.py
    M1L3 tentang bot discord

---
+++ READ.ME.py
@@ -0,0 +1,25 @@
+import discord
+from M1L2_PassGen_main import gen_pass
+# Variabel intents menyimpan hak istimewa bot
+intents = discord.Intents.default()
+# Mengaktifkan hak istimewa message-reading
+intents.message_content = True
+# Membuat bot di variabel klien dan mentransfernya hak istimewa
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Kita telah masuk sebagai {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$halo'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTMyMzIxOTU3Nzk3MDM2MDMzMA.GZP5nA.5B2pTVMP5T6EcIlsCwzL9DZHe-4_c9Aqj7msLc")
